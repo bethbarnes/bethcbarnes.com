812a8e5a776370fb539d6413056750edad8ff646
Beth Barnes <bethcbarnes@outlook.com>
2018-05-07 11:11:41 -0400

    test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//test that this is working
+
 class App extends Component {
   render() {
     return (
