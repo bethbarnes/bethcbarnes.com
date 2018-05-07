2a23d87702cc96e90e90e0f713f5a6509772f494
Beth Barnes <bethcbarnes@outlook.com>
2018-05-07 11:19:54 -0400

    feat(Home) add Home component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-//test that this is working
+import { Home } from './index'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Home />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import Home from './Home';
+
+export { Home }
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
