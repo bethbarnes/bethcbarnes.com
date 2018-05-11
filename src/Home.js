f33e7f4b16e3499d92fe3d811550221f916d87cf
Beth Barnes <bethcbarnes@outlook.com>
2018-05-11 12:54:02 -0400

    feat(Home): resizes photo

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+// import beth from './beth-photo.jpg'
 
 class Home extends Component {
   render() {
     return (
       <div className="App">
         <div className="name-picture-container">
+        <div>
           <img className="front-page-profile-image"
-            src="../public/Beth-Photo-2.jpg"
+            src='/beth-photo.jpg'
             alt="beth-profile"
-
             />
+            </div>
+            <div>
           <h5 className="large-name"> // Beth Barnes </h5>
+          </div>
 
         </div>
       </div>
