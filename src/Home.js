dc441eb980683dd14d7e0cf049424ba5c98c07e2
Beth Barnes <bethcbarnes@outlook.com>
2018-05-12 14:32:38 -0400

    feat(Home): add styling to photo and add buttons

--- src/Home.js
+++ src/Home.js
@@ -7,16 +7,21 @@ class Home extends Component {
     return (
       <div className="App">
         <div className="name-picture-container">
-        <div>
-          <img className="front-page-profile-image"
-            src='/beth-photo.jpg'
-            alt="beth-profile"
+          <div className="name-picture-item">
+            <img className="front-page-profile-image"
+              src='/beth-photo.jpg'
+              alt="beth-profile"
             />
-            </div>
-            <div>
-          <h5 className="large-name"> // Beth Barnes </h5>
           </div>
-
+          <div className="name-picture-item">
+            <h5 className="large-name">
+              {'// Beth Barnes'} </h5>
+          </div>
+        </div>
+        <div className="btn-container">
+          <button className="btn">About</button>
+          <button className="btn">Projects</button>
+          <button className="btn">Contact</button>
         </div>
       </div>
     );
