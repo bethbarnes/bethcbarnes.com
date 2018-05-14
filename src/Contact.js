3229f6723676bd58983847336dfaa8c3fd48bb1d
Beth Barnes <bethcbarnes@outlook.com>
2018-05-14 16:39:41 -0400

    feat(Contact): added contact info

--- src/Contact.js
+++ src/Contact.js
@@ -2,20 +2,23 @@ import React, { Component } from 'react'
 import './App.css'
 import { Navbar } from './index'
 
-class Contact extends Component{
-  render(){
+class Contact extends Component {
+  render() {
     return (
       <div className="contact">
         <h1 className="contact-text" >CONTACT</h1>
-        <h4> EMAIL: </h4>
-        <h4> RESUME: </h4>
+        <div className="contact-info-container" >
+          <h4 className="contact-types"> EMAIL: </h4>
+          <h3> bethcbarnes@outlook.com </h3>
+          <h4 className="contact-types"> RESUME: </h4>
+        </div>
         <div className="icon-container">
-        <a href="https://github.com/bethbarnes" >
-          <i class="fa fa-github small-icon"></i>
-        </a>
-        <a href="https://www.linkedin.com/in/bethcbarnes/" >
-          <i class="fa fa-linkedin-square small-icon"></i>
-        </a>
+          <a href="https://github.com/bethbarnes" >
+            <i class="fa fa-github small-icon"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/bethcbarnes/" >
+            <i class="fa fa-linkedin-square small-icon"></i>
+          </a>
         </div>
       </div>
     )
