800ec63a414d867ebc38b1a39570c87b017f8bd7
Beth Barnes <bethcbarnes@outlook.com>
2018-05-20 11:34:58 -0400

    fix(about): fix typo

--- src/About.js
+++ src/About.js
@@ -11,7 +11,7 @@ class About extends Component {
                 alt="desktop"
                 src="/desktop-image.jpg"/>
           <p className="about-me-text gray-border">
-          A little bit about me: I'm a Javascript developer, cat person and foreign language enthusiast! My interest in tech began when I was in Beijing, studying Mandarin Chinese and had an amazing internship at a iOS app start-up. From there, I worked in sales at a software company and then learned Javascript at Fullstack Academy of Code. I'm most experienced with Javascript, Node.js, React, Redux and Express, but I love experimenting with new technologies like React Native, Google Firebase and ViroReact.
+          A little bit about me: I'm a Javascript developer, cat person and foreign language enthusiast! My interest in tech began when I was in Beijing, studying Mandarin Chinese and had an amazing internship at an iOS app start-up. From there, I worked in sales at a software company and then learned Javascript at Fullstack Academy of Code. I'm most experienced with Javascript, Node.js, React, Redux and Express, but I love experimenting with new technologies like React Native, Google Firebase and ViroReact.
         <br/>
         <br/>
         In my free time, I like to get involved in the tech community, volunteering or attending events from organizations like Girl Develop it and React NYC. I'm also a co-organizer of HackerNest's monthly Tech Socials, which is part of HackerNest's mission to promote tech communities across the world.  If you'd like to get in touch, feel free to contact me!
