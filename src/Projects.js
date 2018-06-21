590a21509509e5cf5f9e1ae880df0d763e151d8f
Beth Barnes <bethcbarnes@outlook.com>
2018-06-20 22:05:51 -0400

    feat(Slides): implement slides

--- src/Projects.js
+++ src/Projects.js
@@ -4,6 +4,26 @@ import projectData from './Data.js'
 
 
 class Projects extends Component {
+
+  componentDidMount(){
+    var slideIndex = 1;
+    showDivs(slideIndex);
+
+    function plusDivs(n) {
+      showDivs(slideIndex += n);
+    }
+
+    function showDivs(n) {
+      var i;
+      var x = document.getElementsByClassName("slide");
+      if (n > x.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = x.length}
+      for (i = 0; i < x.length; i++) {
+         x[i].style.display = "none";
+      }
+      x[slideIndex-1].style.display = "block";
+    }
+  }
   render() {
     return (
       <div className="projects">
@@ -35,7 +55,7 @@ class Projects extends Component {
                 alt={`${project.name} + ${image}`}/>)
               })}
 
-
+              <button>click</button>
             </div> : <div></div>}
 
             <div className="proj-links-container" >
